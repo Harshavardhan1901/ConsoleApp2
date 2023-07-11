015ee5986bd70affaa333098c7a8c6cc985dadee
harsha1901@yahoo.com <harsha1901@yahoo.com>
2023-07-11 10:26:17 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Id;
+            string empName;
+            string empGender;
+            double empSal;
+            DateTime doj;
+            Console.WriteLine("Enter Employee ID");
+            Id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Employee Name");
+            empName = Console.ReadLine();
+            Console.WriteLine("Enter Employee Gender");
+            empGender = Console.ReadLine();
+            Console.WriteLine("Enter Employee Salary");
+            empSal = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Date of Joining");
+            doj = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("ID\t" + "Employee Name\t" + "Employee Gender\t" +
+                "Employee Salary\t" + "Date of Joining");
+            Console.WriteLine(Id + "\t" + empName + "\t" + empGender + "\t" + empSal + "\t" + doj);
+            if (empSal > 90000)
+            {
+                Console.WriteLine("You have to pay 30%tax");
+            }
+            else
+            {
+                Console.WriteLine("You have to pay 15% tax");
+            }
+            Console.ReadKey();
+        }
+    }
+}
